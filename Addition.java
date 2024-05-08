596422c785f0b7c70a03314a2ed4222b0966e4e9
Hardik <hardikmm2121@gmail.com>
2024-05-08 16:38:49 +0530

    Fixed the defect #324 for Addition syntax

---
+++ Addition.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Addition of 3 and 7 is " + (3 + 7));
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Please enter size of the number");
+		int size = sc.nextInt();
+		System.out.println("Please enter number");
+		int a = sc.nextInt();
+		
+		int sum = 0;
+		for (int i = 0; i < size; i++) {
+
+			sum = (a % 10) + sum;
+			a = a / 10;
+
+		}
+		System.out.println(sum);
+	}
+
+}
